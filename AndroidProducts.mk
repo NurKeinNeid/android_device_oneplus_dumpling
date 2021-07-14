20edd11d87ec09bfe7e217d19409f2fdd5fedb9f
NurKeinNeid <mralexman3000@gmail.com>
2021-07-14 20:13:50 +0200

    dumpling: exthm-ify
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/exthm_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    exthm_dumpling-user \
+    exthm_dumpling-userdebug \
+    exthm_dumpling-eng
